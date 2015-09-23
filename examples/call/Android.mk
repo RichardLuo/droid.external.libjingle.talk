9585fd04b73a9333b884ffd26c2c191bf5efd3d7
rxwen <rx.wen218@gmail.com>
2015-09-23 12:41:53 +0800

    Add pthread lib for simulator build
    Change-Id: I33201144a6a45caf935c9345f1df343c7ac16c68

--- examples/call/Android.mk
+++ examples/call/Android.mk
@@ -91,6 +91,8 @@ ifneq ($(TARGET_SIMULATOR),true)
 LOCAL_C_INCLUDES += bionic		# very important!
 LOCAL_C_INCLUDES += external/stlport/stlport 
 LOCAL_SHARED_LIBRARIES += libstlport libdl
+else
+LOCAL_LDLIBS += -lpthread
 endif
 
 
